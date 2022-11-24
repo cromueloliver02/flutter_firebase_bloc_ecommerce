6400936e3be2899ba3a806c333809f66408158fd
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-25 01:01:30 +0800

    fixed exm bottom app bar

--- lib/widgets/ecm_bottom_appbar.dart
+++ lib/widgets/ecm_bottom_appbar.dart
@@ -9,31 +9,29 @@ class ECMBottomAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      child: Container(
-        color: Colors.black,
-        child: SizedBox(
-          height: 70,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                onPressed: () => Navigator.pushNamed(context, HomePage.id),
-                color: Colors.white,
-                icon: const Icon(Icons.home),
-              ),
-              IconButton(
-                onPressed: () => Navigator.pushNamed(context, CartPage.id),
-                color: Colors.white,
-                icon: const Icon(Icons.shopping_cart),
-              ),
-              IconButton(
-                onPressed: () => Navigator.pushNamed(context, UserPage.id),
-                color: Colors.white,
-                icon: const Icon(Icons.person),
-              ),
-            ],
-          ),
+    return Container(
+      color: Colors.black,
+      padding: const EdgeInsets.only(top: 10),
+      child: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () => Navigator.pushNamed(context, HomePage.id),
+              color: Colors.white,
+              icon: const Icon(Icons.home),
+            ),
+            IconButton(
+              onPressed: () => Navigator.pushNamed(context, CartPage.id),
+              color: Colors.white,
+              icon: const Icon(Icons.shopping_cart),
+            ),
+            IconButton(
+              onPressed: () => Navigator.pushNamed(context, UserPage.id),
+              color: Colors.white,
+              icon: const Icon(Icons.person),
+            ),
+          ],
         ),
       ),
     );
