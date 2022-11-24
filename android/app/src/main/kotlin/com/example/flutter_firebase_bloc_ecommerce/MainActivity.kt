34a0e020df222c7054e5920d72482329b722ecc0
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-24 13:22:33 +0800

    init app

---
+++ android/app/src/main/kotlin/com/example/flutter_firebase_bloc_ecommerce/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_firebase_bloc_ecommerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ecommerce_app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('ECOMMERCE APP'),
+        ),
+      ),
+    );
+  }
+}
