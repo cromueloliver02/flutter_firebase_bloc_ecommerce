707446d38ae8b80ab16376567bf8c5ae55a3446e
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-27 16:29:53 +0800

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import './firebase_options.dart';
 // import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'ecommerce_app.dart';
 // import 'utils/utils.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // Bloc.observer = SimpleBlocObserver(); // disable temporarily
 
   runApp(EcommerceApp());
